1512a33273a1bf34b01d78b8257da5668cb38297
Rizwan Iliyas <web4mybiz@gmail.com>
2024-05-06 06:13:13 +0530

    Basic header fields

--- irizweb-data-table.php
+++ irizweb-data-table.php
@@ -4,13 +4,13 @@
 * Plugin URI: https://github.com/web4mybiz/irizweb-data-table
 * Description: Plugin to create a custom table and perform CRUD operations with it.
 * Version: 1.0
-* Requires at least: 
-* Requires PHP:
+* Requires at least: 3.0
+* Requires PHP: 5.0
 * Author: Rizwan Iliyas
 * Author URI: https://github.com/web4mybiz
-* License:  GPLv2
+* License:  GPLv2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Update URI:
 * Text Domain: irizweb-data-table
-* Domain Path: 
+* Domain Path: /languages
  */
